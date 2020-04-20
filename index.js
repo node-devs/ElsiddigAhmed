facceea2fb3f3046cb2964347a043fbf7b07005f
parsercoder <parsercoder@gmail.com>
2020-04-20 23:43:18 +0200

    session 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express")
 const app = express()
 
-app.get("/", function (req, res) {
-  res.send("welcome")
+app.get("/:name/:age", function (req, res) {
+  res.send("welcome " + req.params.name + " you are " + req.params.age)
 })
+
+// app.get("/", function (req, res) {
+//   res.send("welcome " + req.query.name)
+// })
 
 app.listen(1234)
 console.log("server start")
