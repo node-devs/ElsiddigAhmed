1730adf764e3feafb154d073ae17488eb231d419
parsercoder <parsercoder@gmail.com>
2020-05-03 00:34:25 +0200

    adding query by filter

--- routes.js
+++ routes.js
@@ -24,4 +24,11 @@ router.get('/getUsers', function (req, res) {
   users.catch((err) => res.send(err))
 })
 
+router.get('/getUsers/:id', function (req, res) {
+  const id = req.params.id
+  const user = userModel.findById({ _id: id }).exec()
+  user.then((data) => res.json(data))
+  user.catch((err) => res.json(err))
+})
+
 module.exports = router
